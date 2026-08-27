92039ec6a5d038ef1a9e26c09ed315e7e4431775
bacuna02 <bacuna02@ucvvirtual.edu.pe>
2026-08-27 18:48:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 app = Flask(__name__)
 
 # Coordenadas del taller (ejemplo: Villa María del Triunfo, Lima)
-TALLER_LAT = -12.173045093925491
-TALLER_LON = -76.91688622585391
+TALLER_LAT = -12.218535
+TALLER_LON = -76.908586
 RADIO_PERMITIDO = 0.05  # en km (50 metros)
 
 # Definir zona horaria de Lima
